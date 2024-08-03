2d1fbb16a56e31c5e26f1a4a4311b231e88c1a7c
hei karata <herishine00@gmail.com>
2024-08-03 16:35:26 +0300

    Introduce Orleans-based structure for shopping cart app
    Added new Orleans-based project structure:
    - Updated GrainInterfaces.csproj with Orleans SDK reference.
    - Extended ICart interface with Orleans grain methods.
    - Implemented CartGrain class with method stubs.
    - Updated Grains.csproj with necessary package and project references.
    - Added new projects to ShoppingCart.sln and configured build settings.
    - Set up Orleans host in Program.cs with local clustering and logging.
    - Added necessary package references in Silo.csproj.

--- GrainInterfaces/ICart.cs
+++ GrainInterfaces/ICart.cs
@@ -1,6 +1,20 @@
 ﻿namespace GrainInterfaces;
 
-public interface ICart
+[Alias("GrainInterfaces.ICart")]
+public interface ICart : IGrainWithGuidKey
 {
+    [Alias("AddToCart")]
+    ValueTask<Dictionary<string, int>> AddToCart(string product, int quantity);
     
+    [Alias("GetCartContents")]
+    ValueTask<Dictionary<string, int>> GetCartContents();
+    
+    [Alias("RemoveFromCart")]
+    ValueTask<Dictionary<string, int>> RemoveFromCart(string product, int quantity);
+    
+    [Alias("ClearCart")]
+    ValueTask ClearCart();
+    
+    [Alias("Checkout")]
+    ValueTask<string> Checkout();
 }
--- Grains/CartGrain.cs
+++ Grains/CartGrain.cs
@@ -1,6 +1,31 @@
-﻿namespace Grains;
+﻿using GrainInterfaces;
 
-public class CartGrain
+namespace Grains;
+
+public class CartGrain : ICart
 {
-    
+    public async ValueTask<Dictionary<string, int>> AddToCart(string product, int quantity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<Dictionary<string, int>> GetCartContents()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<Dictionary<string, int>> RemoveFromCart(string product, int quantity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask ClearCart()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async ValueTask<string> Checkout()
+    {
+        throw new NotImplementedException();
+    }
 }
